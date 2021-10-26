0ac708968cb7dfc71f1cdaf3c7953aa65782f7b3
Arthur <arthur.baude@in-tech.com>
2021-10-26 10:54:46 +0200

    fix: increase timeouts to prevent fc on nfc intent

--- src/android/src/com/chariotsolutions/nfc/plugin/NfcPlugin.java
+++ src/android/src/com/chariotsolutions/nfc/plugin/NfcPlugin.java
@@ -727,6 +727,17 @@ public class NfcPlugin extends CordovaPlugin implements NfcAdapter.OnNdefPushCom
         });
     }
 
+    private static void wait(int ms) {
+        try
+        {
+            Thread.sleep(ms);
+        }
+        catch(InterruptedException ex)
+        {
+            Thread.currentThread().interrupt();
+        }
+    }
+
     // Send the event data through a channel so the JavaScript side can fire the event
     private void sendEvent(String type, JSONObject tag) {
 
@@ -737,7 +748,15 @@ public class NfcPlugin extends CordovaPlugin implements NfcAdapter.OnNdefPushCom
 
             PluginResult result = new PluginResult(PluginResult.Status.OK, event);
             result.setKeepCallback(true);
-            channelCallback.sendPluginResult(result);
+
+            if (channelCallback != null) {
+                channelCallback.sendPluginResult(result);
+            }
+            else {
+                wait(1000);
+                channelCallback.sendPluginResult(result);
+            }
+
         } catch (JSONException e) {
             Log.e(TAG, "Error sending NFC event through the channel", e);
         }
